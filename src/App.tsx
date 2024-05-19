d8d535c160ef690c6ade2b702bb278acdb03e218
Your Name <jonas.carrillo14@gmail.com>
2024-05-19 12:10:40 -0500

    Agregado el componente Footer con estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Nav from './components/navBar';
+import Footer from './components/footer';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@300" rel="stylesheet" />
       <Nav />
+      <Footer />
     </>
   )
 }
